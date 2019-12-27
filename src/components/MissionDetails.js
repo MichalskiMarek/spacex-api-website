f618ed5ec70d6bcd3db9f1ad94aedeba02ec52b5
Marek <michalskimarek89@gmail.com>
2019-12-27 11:21:01 +0100

    add launch details styles

--- src/components/MissionDetails.js
+++ src/components/MissionDetails.js
@@ -27,18 +27,58 @@ const StyledDiv = styled.div`
   left: 50%;
   transform: translate(-50%, -50%);
   width: 50%;
-  height: 70%;
   background-color: #fff;
   display: flex;
-  flex-direction: column;
+  align-items: flex-start;
+  padding: 10px;
+  box-shadow: 0 2px 20px 0 rgba(0, 0, 0, 0.75);
+`;
+
+const StyledP = styled.p`
+  margin: 5px;
+`;
+
+const StyledSpan = styled.span`
+  font-weight: bold;
+`;
+
+const StyledSection = styled.section`
+  padding: 20px;
+`;
+
+const StyledH3 = styled.h3`
+  margin: 10px 5px;
 `;
 
 const MissionDetails = ({ launch }) => {
+  const launchDate = new Date(launch.launch_date_local);
+
   return (
     <StyledWrapper>
       <StyledDiv>
-        <MissionPatch missionPatch={launch.links.mission_patch} />
-        {launch.flight_number}. {launch.mission_name}
+        <StyledSection>
+          <MissionPatch missionPatch={launch.links.mission_patch} />
+          <StyledP>
+            <StyledSpan>Mission number: </StyledSpan>
+            {launch.flight_number}
+          </StyledP>
+          <StyledP>
+            <StyledSpan>Launch date: </StyledSpan>
+            {launchDate.toLocaleDateString()}
+          </StyledP>
+          <StyledP>
+            <StyledSpan>Rocket used: </StyledSpan>
+            {launch.rocket.rocket_name}
+          </StyledP>
+        </StyledSection>
+        <StyledSection>
+          <StyledH3>{launch.mission_name}</StyledH3>
+          <StyledP>
+            <StyledSpan>Launch site: </StyledSpan>
+            {launch.launch_site.site_name_long}
+          </StyledP>
+          {launch.details ? <StyledP>{launch.details}.</StyledP> : null}
+        </StyledSection>
         <StyledLink to="/">Close</StyledLink>
       </StyledDiv>
     </StyledWrapper>
