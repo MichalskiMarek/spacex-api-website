04d1d703fe28e2ba1337700c31042bbf4779ce0e
Marek <michalskimarek89@gmail.com>
2019-12-30 11:50:58 +0100

    Updates

---
+++ precache-manifest.4d83172de27111e327a0a49ecea2aac0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b124ce0a601e748da16e7b35cb2426a",
+    "url": "/spacex-api-website/index.html"
+  },
+  {
+    "revision": "e29c378dff4c961eebad",
+    "url": "/spacex-api-website/static/js/2.4bcbb0eb.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/spacex-api-website/static/js/2.4bcbb0eb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7bd0b13e62526d11af0d",
+    "url": "/spacex-api-website/static/js/main.11184944.chunk.js"
+  },
+  {
+    "revision": "dd81393d767cb2b80774",
+    "url": "/spacex-api-website/static/js/runtime-main.5332937e.js"
+  },
+  {
+    "revision": "fac1de7c537e7cd4105badb6282d807e",
+    "url": "/spacex-api-website/static/media/default_mission_patch.fac1de7c.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.341e04d9aa859aec9e1abe477e91189b.js"
+  "/spacex-api-website/precache-manifest.4d83172de27111e327a0a49ecea2aac0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/spacex-api-website/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
